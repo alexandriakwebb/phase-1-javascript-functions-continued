b27d406bce1a0578c48471ce9bcbeb74f669a774
Alexandria Webb <alexandriakwebb@gmail.com>
2023-09-01 07:39:44 -0600

    writes functions to pass all tests.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(mondayActivity = `go to the office`) {
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(visualFlair = `*`) {
+    return function(enterAdj = `special`) {
+        return `You are ${visualFlair}${enterAdj}${visualFlair}!`;
+    }
+
+}
